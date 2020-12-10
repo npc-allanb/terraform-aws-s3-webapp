e9eb98a8c3a1ddd91bb3e1cf131c7d384a65ac3e
npc-allanb <allanb@newspaperclub.com>
2020-12-10 19:27:56 +0000

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "endpoint" {
   value = aws_s3_bucket.bucket.website_endpoint
 }
+
+output "arn" {
+  value = aws_s3_bucket.bucket.arn
+}
+
+output "endpoint" {
+  value = aws_s3_bucket.bucket.bucket
+}
